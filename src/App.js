53969b5ccf997425f63a5d41cb8bea4db24f1963
Buddhi Raj Gurung <rajbuddhi496@gmail.com>
2023-11-21 20:01:13 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import myphoto from './image/myphoto.jpg'
+import Popup from 'reactjs-popup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='popups'>
+            <h4>Popup Screen</h4>
+            <Popup trigger=
+                {<img src={myphoto} alt="" />} 
+                modal nested>
+                {
+                    close => (
+                        <div className='modal'>
+                            <div className='content'>
+                              <img src={myphoto} alt="" />
+                            </div>
+                            <div>
+                                <button onClick=
+                                    {() => close()}>
+                                        close
+                                </button>
+                            </div>
+                        </div>
+                    )
+                }
+            </Popup>
+        </div>
   );
 }
 
